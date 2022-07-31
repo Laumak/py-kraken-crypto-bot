27d6c65e0a69db24e2fcf445a9663d75e0c6ae9d
Lauri Mäkinen <latemak@gmail.com>
2022-07-31 15:59:24 +0300

    Code cleanup

--- app/main.py
+++ app/main.py
@@ -14,6 +14,9 @@ with open(".env", "r") as f:
   api_key = lines[0]
   api_secret = lines[1]
 
+def get_utc_time_in_milliseconds():
+  return str(int(1000 * time.time()))
+
 def get_kraken_signature(urlpath, data, secret):
     postdata = urllib.parse.urlencode(data)
     encoded = (str(data['nonce']) + postdata).encode()
@@ -23,7 +26,7 @@ def get_kraken_signature(urlpath, data, secret):
     sigdigest = base64.b64encode(mac.digest())
     return sigdigest.decode()
 
-def kraken_request(url_path, data, api_key, api_secret):
+def kraken_request(url_path, data):
   headers = {
     "API-Key": api_key,
     "API-Sign": get_kraken_signature(url_path, data, api_secret)
@@ -32,9 +35,7 @@ def kraken_request(url_path, data, api_key, api_secret):
 
 resp = kraken_request(
   "/0/private/Balance",
-  { "nonce": str(int(1000 * time.time())) },
-  api_key,
-  api_secret
+  { "nonce": get_utc_time_in_milliseconds() }
 )
 
 print(resp.json())
